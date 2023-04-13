986ee6aba6350dbf843bc698b675953bd88e6510
Catalina Lorca <catalina.lorca_b@mail.udp.cl>
2023-04-12 23:19:38 -0400

    guardar data en redis

--- src/index.js
+++ src/index.js
@@ -1,45 +1,63 @@
 import fetch from 'node-fetch';
-import { writeFileSync } from 'fs';
+import { createClient } from 'redis';
 
+const endPoints = ["action-adventura", "animation", "classic", "comedy", "drama", 
+                  "horror", "family", "mystery", "scifi-fantasy", "western"]
 
 async function fetchData() {
   try {
-    const response = await fetch('https://api.sampleapis.com/movies/drama');
+    const response = await fetch('https://api.sampleapis.com/movies/family');
     if (!response.ok) {
       throw new Error('Error en la respuesta del servidor: ' + response.status);
     }
-    const data = await response.json();
-    //console.log(data);
+    const content = await response.text();
+    // console.log(content);
+    await redis1.set('family', JSON.stringify(content));
+    // redis1.set('family', JSON.stringify(content), (err, reply) => {
+    //   if (err) {
+    //     console.error('Error al establecer el valor en Redis:', err);
+    //     return;
+    //   }
+    //   console.log('Valor establecido en Redis:', reply);
+    // });
+    //const contenido = await response.json();
+    // console.log(contenido);
   } catch (error) {
     console.error('Error al obtener los datos: ' + error);
   }
 }
 
-async function makeRequests(n) {
-  const responseTimes = [];
-  for (let i = 0; i < n; i++) {
-    const startTime = new Date().getTime();
-    await fetchData();
-    const endTime = new Date().getTime();
-    const responseTime = endTime - startTime;
-    console.log("Petición " + (i+1) + ": " + responseTime + " ms"); 
-    responseTimes.push(responseTime);
-  }
-  return responseTimes;
+async function dataRedis() {
+  await redis1.connect();
+  await fetchData();
+  const value = await redis1.get('family');
+
+  console.log("********** Respuesta REDIS *********: " + value);
+  await redis1.disconnect();
 }
 
-const n = 100; // número de consultas
-makeRequests(n)
-  .then(responseTimes => {
-    console.log('Arreglo de tiempos de respuesta:', responseTimes);
-    const contenido = responseTimes.join('\n');
-    writeFileSync('response_times.txt', contenido, err => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-    })
-  })
-  .catch(error => {
-    console.error('Error al obtener los datos: ' + error);
-  });
+function generateEndPoints(){
+  // Asegúrate de que min y max sean enteros
+  min = Math.ceil(0);
+  max = Math.floor(9);
+  
+  // Genera un número aleatorio entre min y max (inclusive)
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  
+}
+
+const redis1 = createClient({host: 'localhost', port: 6379,});
+const redis2 = createClient({host: 'localhost', port: 6380,});
+const redis3 = createClient({host: 'localhost', port: 6381,});
+
+redis1.on('error', err => console.log('Redis Client Error', err));
+redis2.on('error', err => console.log('Redis Client Error', err));
+redis3.on('error', err => console.log('Redis Client Error', err));
+
+dataRedis();
+
+
+
+
+
+
